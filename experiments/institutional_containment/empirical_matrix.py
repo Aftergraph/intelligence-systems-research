787d9ae7116973dce770e3634a8ba544d13e483c
Jonas Abde <147070826+JonasAbde@users.noreply.github.com>
2026-09-09 17:02:17 +0200

    test(study012b): make B1 RED boundary explicit

---
+++ experiments/institutional_containment/empirical_matrix.py
@@ -0,0 +1,7 @@
+"""Placeholder module for STUDY-012B B1 paired empirical matrix TDD RED phase."""
+
+B1_CONDITIONS = ("I0", "I3")
+
+
+def run_paired_empirical_matrix(*args, **kwargs):
+    raise NotImplementedError("B1 paired empirical matrix not implemented")
