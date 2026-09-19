91963aba6bd36cdad95006b9a2cfb5d961319956
Jonas Abde <147070826+JonasAbde@users.noreply.github.com>
2026-09-19 10:25:34 +0200

    fix(verge): correct Bastion package initializer

--- experiments/verge_bastion/__init__.py
+++ experiments/verge_bastion/__init__.py
@@ -1,1 +1,4 @@
-"""JAR-EXP-0018 VERGE Bastion robust-evolution research."""\n\nEXPERIMENT_ID = "JAR-EXP-0018"\nALGORITHM_NAME = "VERGE Bastion"\n
+"""JAR-EXP-0018 VERGE Bastion robust-evolution research."""
+
+EXPERIMENT_ID = "JAR-EXP-0018"
+ALGORITHM_NAME = "VERGE Bastion"
